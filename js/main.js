81a9f9e656df33b875d0d5d68e1a102675d761c0
Ashkan <dhs.ashkan@gmail.com>
2018-06-19 20:07:29 +0430

    initial commit

---
+++ js/main.js
@@ -0,0 +1,56 @@
+$(document).on('click', '#submit', function () {
+    calculation();
+})
+
+function calculation() {
+    var a = parseInt($('#charge_one').val().trim());
+    var a_type = parseFloat($('#type_one').val());
+    var b = parseInt($('#charge_two').val().trim());
+    var b_type = parseFloat($('#type_two').val());
+    var c = parseInt(Math.pow($("#distance").val().trim(), 2));
+    var answer = parseFloat((9988000000 * (a * a_type) * (b * b_type)) / c * 0.04);
+    $('#answer').text(answer + ' n');
+    if($('#answer').text() != "NaN n"){
+        animation();
+    } else {
+        $('.card').removeClass('fadeInUp').addClass('shake')
+        $("#submit").prop('disabled', true)
+        setTimeout(function () {
+            $("#submit").prop('disabled', false)
+            $('.card').removeClass('shake')
+        }, 1000)
+    }
+}
+
+function animation(){
+    $('#inputs').fadeOut(200);
+    $("#result div").animate({
+        top: '60%',
+        opacity: '1',
+    }, 1000);
+}
+
+function return_animation(){
+    $("#result div").animate({
+        top: '100%',
+        opacity: '0',
+    }, 200);
+    $('#inputs').fadeIn(800);
+}
+
+$(document).on('keydown', '#charge_one, #charge_two, #distance', function (e) {
+    if(e.keyCode == 13){
+        if($('#charge_one').val().trim() != '' && $('#charge_two').val().trim() != '' && $('#distance').val().trim() != ''){
+            calculation();
+        }
+    }
+})
+
+$(document).on('click', '#back', function () {
+    $('#charge_one').val('');
+    $('#charge_two').val('');
+    $("#distance").val('');
+    $('.selectpicker').selectpicker('val', '');
+    $('#charge_one, #charge_two, #distance').parent().removeClass('is-filled')
+    return_animation();
+})
